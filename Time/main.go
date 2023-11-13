eab3f58900139abff8097fd04ca43742082b4629
Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
2023-11-13 14:03:59 +0530

    test commit

--- Time/main.go
+++ Time/main.go
@@ -9,4 +9,6 @@ func main() {
 	timing := time.Date(2022, time.January, 10, 10, 23, 56, 56, time.UTC)
 	fmt.Println(timing.Format("01-02-2006 Monday"))
 
+	timing := time.Date(2022, time.January, 10, 10, 23, 56, 56, time.UT)
+	fmt.Println(timing.Format("01-02-2006 Monday"))
 }
